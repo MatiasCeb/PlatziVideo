136bbf2cbe61deab0fab46bce68a79dec7a4d015
MatiasCeb <matiasceb8812@gmail.com>
2021-06-27 21:15:27 -0300

    quit FakeAPI

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,22 +5,20 @@ import Categories from "../components/Categories";
 import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
-import useInitialState from "../hooks/useInitialState.js";
+import PropTypes from "../shared/initialState";
 import "../assets/styles/App.scss";
 
-const API = "http://localhost:3000/initalState";
-
 const App = () => {
-    const initialState = useInitialState(API);
-    return initialState.length === 0 ? <h1>Loading...</h1> : (
+    const initialState = initialState;
+    return PropTypes.mylist.length === 0 ? <h1>Loading...</h1> : (
         <div className="App">
             <Header />
             <Search />
 
-            {initialState.mylist.length > 0 &&
+            {PropTypes.mylist.length > 0 &&
                 <Categories title="Mi lista">
                     <Carousel>
-                        {initialState.mylist.map(item => 
+                        {PropTypes.mylist.map(item => 
                             <CarouselItem key={item.id} {...item} />
                         )}
                     </Carousel>
@@ -29,7 +27,7 @@ const App = () => {
 
             <Categories title="Tendencias">
                 <Carousel>
-                    {initialState.trends.map(item => 
+                    {PropTypes.trends.map(item => 
                         <CarouselItem key={item.id} {...item} />
                     )}
                 </Carousel>
@@ -37,7 +35,7 @@ const App = () => {
 
             <Categories title="Originales de Platzi Video">
                 <Carousel>
-                    {initialState.originals.map(item => 
+                    {PropTypes.originals.map(item => 
                         <CarouselItem key={item.id} {...item} />
                     )}
                 </Carousel>
